ad6383f5db83f44f0bf8ffb24927548e2c14c128
edramirez <edramirez@itcr.ac.cr>
2020-11-17 10:59:25 -0600

    Actualización al P06

--- Python/p99.py
+++ Python/p99.py
@@ -8,4 +8,21 @@ def p02(L):
 def p03(k,L):
     return L[k]
 
+# Length of a list, recursive version
+def p04(L):
+    if(L==[]):
+        return 0
+    return 1+p04(L[1:])
+
+# Reverse a list 
+def p05(L):
+    return reverse(L,[])
+
+def reverse(L,L2):
+    if(L == []):
+        return L2
+    L2.append(L[0])
+    return reverse(L[1:], L2)
+
+
 
